85383facbd5b1dc028a7c2dceb85e9b52cfb72f1
Uri Shaked <uri@urishaked.com>
2024-09-30 13:23:19 +0000

    feat: factory test module

--- test/tb.v
+++ test/tb.v
@@ -22,13 +22,13 @@ module tb ();
   wire [7:0] uo_out;
   wire [7:0] uio_out;
   wire [7:0] uio_oe;
-`ifdef GL_TEST
+  `ifdef GL_TEST
   wire VPWR = 1'b1;
   wire VGND = 1'b0;
 `endif
 
-  // Replace tt_um_example with your module name:
-  tt_um_example user_project (
+
+  tt_um_factory_test user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
